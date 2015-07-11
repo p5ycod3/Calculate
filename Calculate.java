e3078ed808fe5f30b88da915072872a4675afb6e
p5ycod3 <8bit.ds@gmail.com>
2015-07-11 16:41:07 +0300

    test args in console

---
+++ Calculate.java
@@ -0,0 +1,19 @@
+/**
+ * Created by psycode on 11.07.15.
+ */
+public class Calculate {
+    public static void main(String[] arg){
+
+        System.out.println("Calculate");
+
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summa = first + second;
+        System.out.println("Summa " + summa);
+
+    }
+
+
+}
+
+
